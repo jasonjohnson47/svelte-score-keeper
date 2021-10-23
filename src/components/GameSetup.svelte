e9171850af208c91580b1189e9c17bb23af805ac
Jason Johnson <jasonjohnson47@gmail.com>
2021-10-23 04:11:54 +0000

    Starting to setup GameSetup component

--- src/components/GameSetup.svelte
+++ src/components/GameSetup.svelte
@@ -3,3 +3,15 @@
 </script>
 
 <h1>Who's Playing?</h1>
+
+<div>
+    <label for="name-1">Player Name</label>
+    <input type="text" id="name-1" value="">
+</div>
+
+<div>
+    <label for="name-2">Player Name</label>
+    <input type="text" id="name-2" value="">
+</div>
+
+<button type="button">Start Game</button>
